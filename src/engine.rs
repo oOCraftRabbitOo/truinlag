92a26835f45bcf0089468bab28f7d16454bd9aad
nelipc <nelio.zoller@gmail.com>
2024-01-10 08:43:02 +0100

    started engine dev

--- src/engine.rs
+++ src/engine.rs
@@ -1,8 +1,15 @@
 use crate::commands::{BroadcastAction, EngineCommand, EngineResponse, ResponseAction};
+use std::path;
 
 pub fn vroom(command: EngineCommand) -> EngineResponse {
     EngineResponse {
         response_action: ResponseAction::Success,
         broadcast_action: Some(BroadcastAction::Success),
     }
+}
+
+trait Session {}
+
+struct Engine {
+    sessions: Vec<Box<dyn Session>>,
 }
