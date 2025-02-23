01e5606092a1f9f04fbf1885e07d1583de865675
oOCraftRabbitOo <77200368+oOCraftRabbitOo@users.noreply.github.com>
2025-02-23 10:36:57 +0100

    some further cleaning

--- src/session.rs
+++ src/session.rs
@@ -9,7 +9,7 @@ use partially::Partial;
 use serde::{Deserialize, Serialize};
 use strsim::normalized_damerau_levenshtein as strcmp;
 use truinlag::{
-    commands::{BroadcastAction, EngineAction, EngineResponse, ResponseAction},
+    commands::{BroadcastAction::*, EngineAction::*, Error::*, ResponseAction::*, *},
     *,
 };
 
@@ -54,134 +54,144 @@ impl Session {
         }
     }
 
-    pub fn vroom(
+    fn generate_team_challenges(
         &mut self,
-        command: EngineAction,
-        session_id: u64,
-        player_entries: &[DBEntry<PlayerEntry>],
+        id: usize,
         challenge_entries: &[DBEntry<ChallengeEntry>],
         zone_entries: &[DBEntry<ZoneEntry>],
     ) -> InternEngineResponsePackage {
-        use commands::Error::*;
-        use BroadcastAction::*;
-        use EngineAction::*;
-        use ResponseAction::*;
-        match command {
-            GenerateTeamChallenges(id) => {
-                let config = self.config();
-                match self.teams.get_mut(id) {
-                    None => Error(NotFound).into(),
-                    Some(team) => {
-                        team.generate_challenges(&config, challenge_entries, zone_entries);
-                        Success.into()
-                    }
-                }
+        let config = self.config();
+        match self.teams.get_mut(id) {
+            None => Error(NotFound).into(),
+            Some(team) => {
+                team.generate_challenges(&config, challenge_entries, zone_entries);
+                Success.into()
             }
-            AddChallengeToTeam { team, challenge } => match self.teams.get_mut(team) {
-                // THIS METHOD SHOULD BE TEMPORARY AND EXISTS ONLY FOR TESTING PURPOSES
-                None => Error(NotFound).into(),
-                Some(team) => {
-                    if self.game.is_some() {
-                        Error(GameInProgress).into()
-                    } else {
-                        team.challenges.push(InOpenChallenge {
-                            title: challenge.title,
-                            description: challenge.description,
-                            points: challenge.points,
-                            action: None,
-                            zone: None,
-                            id: 0,
-                        });
-                        Success.into()
-                    }
-                }
-            },
-            RenameTeam { team, new_name } => match self.teams.get_mut(team) {
-                None => Error(NotFound).into(),
-                Some(team) => {
-                    team.name = new_name;
+        }
+    }
+
+    fn add_challenge_to_team(
+        &mut self,
+        team: usize,
+        challenge: Challenge,
+    ) -> InternEngineResponsePackage {
+        match self.teams.get_mut(team) {
+            // THIS METHOD SHOULD BE TEMPORARY AND EXISTS ONLY FOR TESTING PURPOSES
+            None => Error(NotFound).into(),
+            Some(team) => {
+                if self.game.is_some() {
+                    Error(GameInProgress).into()
+                } else {
+                    team.challenges.push(InOpenChallenge {
+                        title: challenge.title,
+                        description: challenge.description,
+                        points: challenge.points,
+                        action: None,
+                        zone: None,
+                        id: 0,
+                    });
                     Success.into()
                 }
-            },
-            MakeTeamCatcher(id) => match self.teams.get_mut(id) {
-                None => Error(NotFound).into(),
-                Some(team) => match team.role {
-                    TeamRole::Catcher => Success.into(),
-                    TeamRole::Runner => {
-                        team.role = TeamRole::Catcher;
-                        EngineResponse {
-                            response_action: Success,
-                            broadcast_action: Some(TeamMadeCatcher(
-                                team.to_sendable(player_entries, id),
-                            )),
-                        }
-                        .into()
-                    }
-                },
-            },
-            MakeTeamRunner(id) => match self.teams.get_mut(id) {
-                None => Error(NotFound).into(),
-                Some(team) => match team.role {
-                    TeamRole::Runner => Success.into(),
-                    TeamRole::Catcher => {
-                        team.role = TeamRole::Runner;
-                        EngineResponse {
-                            response_action: Success,
-                            broadcast_action: Some(TeamMadeRunner(
-                                team.to_sendable(player_entries, id),
-                            )),
-                        }
-                        .into()
-                    }
-                },
-            },
-            SendLocation { player, location } => {
-                match self
-                    .teams
-                    .iter()
-                    .position(|t| t.players.iter().all(|&p| p == player))
-                {
-                    None => Error(NotFound).into(),
-                    Some(team) => {
-                        self.teams[team].locations.push((
-                            location.0,
-                            location.1,
-                            chrono::offset::Local::now().time(),
-                        ));
-                        EngineResponse {
-                            response_action: Success,
-                            broadcast_action: Some(Location { team, location }),
-                        }
-                        .into()
-                    }
-                }
             }
-            AssignPlayerToTeam { player, team } => {
-                let mut old_team = None;
-                self.teams.iter_mut().enumerate().for_each(|(index, t)| {
-                    if let Some(i) = t.players.iter().position(|p| p == &player) {
-                        t.players.remove(i);
-                        old_team = Some(index)
+        }
+    }
+
+    fn rename_team(&mut self, team: usize, new_name: String) -> InternEngineResponsePackage {
+        match self.teams.get_mut(team) {
+            None => Error(NotFound).into(),
+            Some(team) => {
+                team.name = new_name;
+                Success.into()
+            }
+        }
+    }
+
+    fn make_team_catcher(
+        &mut self,
+        id: usize,
+        player_entries: &[DBEntry<PlayerEntry>],
+    ) -> InternEngineResponsePackage {
+        match self.teams.get_mut(id) {
+            None => Error(NotFound).into(),
+            Some(team) => match team.role {
+                TeamRole::Catcher => Success.into(),
+                TeamRole::Runner => {
+                    team.role = TeamRole::Catcher;
+                    EngineResponse {
+                        response_action: Success,
+                        broadcast_action: Some(TeamMadeCatcher(
+                            team.to_sendable(player_entries, id),
+                        )),
                     }
-                });
-                match team {
-                    Some(t) => match self.teams.get_mut(t) {
-                        Some(t) => {
-                            t.players.push(player);
-                            EngineResponse {
-                                response_action: Success,
-                                broadcast_action: Some(PlayerChangedTeam {
-                                    session: session_id,
-                                    player,
-                                    from_team: old_team,
-                                    to_team: team,
-                                }),
-                            }
-                            .into()
-                        }
-                        None => Error(NotFound).into(),
-                    },
-                    None => EngineResponse {
+                    .into()
+                }
+            },
+        }
+    }
+
+    fn make_team_runner(
+        &mut self,
+        id: usize,
+        player_entries: &[DBEntry<PlayerEntry>],
+    ) -> InternEngineResponsePackage {
+        match self.teams.get_mut(id) {
+            None => Error(NotFound).into(),
+            Some(team) => match team.role {
+                TeamRole::Runner => Success.into(),
+                TeamRole::Catcher => {
+                    team.role = TeamRole::Runner;
+                    EngineResponse {
+                        response_action: Success,
+                        broadcast_action: Some(TeamMadeRunner(
+                            team.to_sendable(player_entries, id),
+                        )),
+                    }
+                    .into()
+                }
+            },
+        }
+    }
+
+    fn send_location(&mut self, player: u64, location: (f64, f64)) -> InternEngineResponsePackage {
+        match self
+            .teams
+            .iter()
+            .position(|t| t.players.iter().all(|&p| p == player))
+        {
+            None => Error(NotFound).into(),
+            Some(team) => {
+                self.teams[team].locations.push((
+                    location.0,
+                    location.1,
+                    chrono::offset::Local::now().time(),
+                ));
+                EngineResponse {
+                    response_action: Success,
+                    broadcast_action: Some(Location { team, location }),
+                }
+                .into()
+            }
+        }
+    }
+
+    fn assign_player_to_team(
+        &mut self,
+        player: u64,
+        team: Option<usize>,
+        session_id: u64,
+    ) -> InternEngineResponsePackage {
+        let mut old_team = None;
+        self.teams.iter_mut().enumerate().for_each(|(index, t)| {
+            if let Some(i) = t.players.iter().position(|p| p == &player) {
+                t.players.remove(i);
+                old_team = Some(index)
+            }
+        });
+        match team {
+            Some(t) => match self.teams.get_mut(t) {
+                Some(t) => {
+                    t.players.push(player);
+                    EngineResponse {
                         response_action: Success,
                         broadcast_action: Some(PlayerChangedTeam {
                             session: session_id,
@@ -190,77 +200,135 @@ impl Session {
                             to_team: team,
                         }),
                     }
-                    .into(),
+                    .into()
                 }
+                None => Error(NotFound).into(),
+            },
+            None => EngineResponse {
+                response_action: Success,
+                broadcast_action: Some(PlayerChangedTeam {
+                    session: session_id,
+                    player,
+                    from_team: old_team,
+                    to_team: team,
+                }),
             }
-            Catch {
-                catcher: _,
-                caught: _,
-            } => Error(NotImplemented).into(), // TODO:
+            .into(),
+        }
+    }
+
+    fn catch(&mut self, catcher: usize, caught: usize) -> InternEngineResponsePackage {
+        Error(NotImplemented).into()
+    }
+
+    fn complete(&mut self, completer: usize, completed: usize) -> InternEngineResponsePackage {
+        match self.teams.get_mut(completer) {
+            Some(completer) => match completer.challenges.get_mut(completed) {
+                Some(_completed) => todo!(),
+                None => todo!(),
+            },
+            None => todo!(),
+        }
+    }
+
+    fn get_state(&self, player_entries: &[DBEntry<PlayerEntry>]) -> InternEngineResponsePackage {
+        SendState {
+            teams: self
+                .teams
+                .iter()
+                .enumerate()
+                .map(|(i, t)| t.to_sendable(player_entries, i))
+                .collect(),
+            game: self.game.clone().map(|g| g.to_sendable()),
+        }
+        .into()
+    }
+
+    fn add_team(
+        &mut self,
+        name: String,
+        discord_channel: Option<u64>,
+        colour: Option<Colour>,
+    ) -> InternEngineResponsePackage {
+        if let Some(nom) = self
+            .teams
+            .iter()
+            .map(|t| t.name.clone())
+            .find(|n| strcmp(&n.to_lowercase(), &name.to_lowercase()) >= 0.85)
+        {
+            Error(TeamExists(nom)).into()
+        } else {
+            let colour = match colour {
+                Some(c) => c,
+                None => {
+                    match self
+                        .config()
+                        .team_colours
+                        .iter()
+                        .find(|&&c| !self.teams.iter().any(|t| t.colour == c))
+                    {
+                        Some(&colour) => colour,
+                        None => Colour { r: 0, g: 0, b: 0 },
+                    }
+                }
+            };
+            self.teams.push(TeamEntry::new(
+                name,
+                Vec::new(),
+                discord_channel,
+                colour,
+                self.config(),
+            ));
+            Success.into()
+        }
+    }
+
+    fn start(&mut self) -> InternEngineResponsePackage {
+        match self.game {
+            Some(_) => Error(GameInProgress).into(),
+            None => {
+                Error(NotImplemented).into() // TODO:
+            }
+        }
+    }
+
+    fn stop(&mut self) -> InternEngineResponsePackage {
+        Error(NotImplemented).into() // TODO:
+    }
+
+    pub fn vroom(
+        &mut self,
+        command: EngineAction,
+        session_id: u64,
+        player_entries: &[DBEntry<PlayerEntry>],
+        challenge_entries: &[DBEntry<ChallengeEntry>],
+        zone_entries: &[DBEntry<ZoneEntry>],
+    ) -> InternEngineResponsePackage {
+        match command {
+            GenerateTeamChallenges(id) => {
+                self.generate_team_challenges(id, challenge_entries, zone_entries)
+            }
+            AddChallengeToTeam { team, challenge } => self.add_challenge_to_team(team, challenge),
+            RenameTeam { team, new_name } => self.rename_team(team, new_name),
+            MakeTeamCatcher(id) => self.make_team_catcher(id, player_entries),
+            MakeTeamRunner(id) => self.make_team_runner(id, player_entries),
+            SendLocation { player, location } => self.send_location(player, location),
+            AssignPlayerToTeam { player, team } => {
+                self.assign_player_to_team(player, team, session_id)
+            }
+            Catch { catcher, caught } => self.catch(catcher, caught),
             Complete {
                 completer,
                 completed,
-            } => match self.teams.get_mut(completer) {
-                Some(completer) => match completer.challenges.get_mut(completed) {
-                    Some(_completed) => todo!(),
-                    None => todo!(),
-                },
-                None => todo!(),
-            },
-            GetState => SendState {
-                teams: self
-                    .teams
-                    .iter()
-                    .enumerate()
-                    .map(|(i, t)| t.to_sendable(player_entries, i))
-                    .collect(),
-                game: self.game.clone().map(|g| g.to_sendable()),
-            }
-            .into(),
+            } => self.complete(completer, completed),
+            GetState => self.get_state(player_entries),
             AddTeam {
                 name,
                 discord_channel,
                 colour,
-            } => {
-                if let Some(nom) = self
-                    .teams
-                    .iter()
-                    .map(|t| t.name.clone())
-                    .find(|n| strcmp(&n.to_lowercase(), &name.to_lowercase()) >= 0.85)
-                {
-                    Error(TeamExists(nom)).into()
-                } else {
-                    let colour = match colour {
-                        Some(c) => c,
-                        None => {
-                            match self
-                                .config()
-                                .team_colours
-                                .iter()
-                                .find(|&&c| !self.teams.iter().any(|t| t.colour == c))
-                            {
-                                Some(&colour) => colour,
-                                None => Colour { r: 0, g: 0, b: 0 },
-                            }
-                        }
-                    };
-                    self.teams.push(TeamEntry::new(
-                        name,
-                        Vec::new(),
-                        discord_channel,
-                        colour,
-                        self.config(),
-                    ));
-                    Success.into()
-                }
-            }
-            Start => match self.game {
-                Some(_) => Error(GameInProgress).into(),
-                None => {
-                    Error(NotImplemented).into() // TODO:
-                }
-            },
-            Stop => Error(NotImplemented).into(), // TODO:
+            } => self.add_team(name, discord_channel, colour),
+            Start => self.start(),
+            Stop => self.stop(),
             AddSession { name: _, mode: _ } => Error(SessionSupplied).into(),
             Ping(_) => Error(SessionSupplied).into(),
             GetPlayerByPassphrase(_) => Error(SessionSupplied).into(),
